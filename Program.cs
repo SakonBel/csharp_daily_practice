e1ad4a1a9d1767e6f7795edeaaacddf5cba8919a
Sakon Plangklang <iamsabell@gmail.com>
2025-10-01 19:38:19 +0700

    Create Celsius to Fahrenheit converter

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿Console.Write("Tell me your name : ");
-string? userName = Console.ReadLine();
-Console.Write("How old are you : ");
-int? userAge = int.Parse(Console.ReadLine());
-
-
-Console.WriteLine($"Hello, {userName}! \nYou are {userAge} years old and can access our program.");
+﻿Console.Write("Please input your Celsius degree : ");
+string? celsiusDegree = Console.ReadLine();
+int fahrenheitDegree = (Convert.ToInt32(celsiusDegree) * (9 / 5)) + 32;
+Console.WriteLine($"Your temperature in Fahrenheit is {fahrenheitDegree} degree!");
