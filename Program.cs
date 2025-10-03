1cd70927621f684935144db178a479c060bf1bfc
Sakon Plangklang <iamsabell@gmail.com>
2025-10-03 23:08:31 +0700

    Make all code into there own class and methods

--- Program.cs
+++ Program.cs
@@ -1,61 +1,85 @@
 ﻿// Create (simple) calculator
-int? firstNumber = null;
-int? secondNumber = null;
-string? oper = null;
-double? result = null;
+Operation oper = new();
+oper.OperationResult();
 
-while (firstNumber == null)
+class Operation
 {
-  Console.Write("Insert your first number : ");
-  try
+  int? firstNumber = null;
+  int? secondNumber = null;
+  string? oper = null;
+  double? result = null;
+  public void GetFirstNumber()
   {
-    firstNumber = int.Parse(Console.ReadLine() ?? "");
+    while (firstNumber == null)
+    {
+      Console.Write("Insert your first number : ");
+      try
+      {
+        firstNumber = int.Parse(Console.ReadLine() ?? "");
+      }
+      catch
+      {
+        Console.WriteLine("First number is not a valid number");
+      }
+    }
   }
-  catch
+
+  public void GetSecondNumber()
   {
-    Console.WriteLine("First number is not a valid number");
+
+    while (secondNumber == null)
+    {
+      Console.Write("Insert your second number : ");
+      try
+      {
+        secondNumber = int.Parse(Console.ReadLine() ?? "");
+      }
+      catch
+      {
+        Console.WriteLine("Second number is not a valid number");
+      }
+    }
+  }
+
+  public void UseOperator()
+  {
+
+    while (oper == null)
+    {
+      Console.Write("Insert operator (+,-,*,/) : ");
+
+      oper = Console.ReadLine();
+      if (oper == "+")
+      {
+        result = firstNumber + secondNumber;
+      }
+      else if (oper == "-")
+      {
+        result = firstNumber - secondNumber;
+      }
+      else if (oper == "*")
+      {
+        result = firstNumber * secondNumber;
+      }
+      else if (oper == "/")
+      {
+        result = (double?)firstNumber / secondNumber;
+      }
+      else
+      {
+        Console.WriteLine("Not a valid operator!");
+        oper = null;
+      }
+
+    }
+  }
+
+  public void OperationResult()
+  {
+    GetFirstNumber();
+    GetSecondNumber();
+    UseOperator();
+    Console.WriteLine($"Your result is : {result}");
   }
 }
 
-while (secondNumber == null)
-{
-  Console.Write("Insert your second number : ");
-  try
-  {
-    secondNumber = int.Parse(Console.ReadLine() ?? "");
-  }
-  catch
-  {
-    Console.WriteLine("Second number is not a valid number");
-  }
-}
-while (oper == null)
-{
-  Console.Write("Insert operator (+,-,*,/) : ");
-
-  oper = Console.ReadLine();
-  if (oper == "+")
-  {
-    result = firstNumber + secondNumber;
-  }
-  else if (oper == "-")
-  {
-    result = firstNumber - secondNumber;
-  }
-  else if (oper == "*")
-  {
-    result = firstNumber * secondNumber;
-  }
-  else if (oper == "/")
-  {
-    result = (double)firstNumber / secondNumber;
-  }
-  else
-  {
-    Console.WriteLine("Not a valid operator!");
-    oper = null;
-  }
-
-}
-
-Console.WriteLine($"Your result is : {result}");
